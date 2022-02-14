8ed28209d956a12dacd1b6eda7b3f15226d4f15d
Anna Epishova <anna.epishova@gmail.com>
2022-02-14 13:04:39 -0500

    Fixed test8

--- td2bq_mapper/tests/test_td2bq.py
+++ td2bq_mapper/tests/test_td2bq.py
@@ -221,8 +221,8 @@ def test_generate_jsons_test8():
     Returns:
       None
     """
-    generate_jsons("test6")
+    generate_jsons("test8")
 
 
 if __name__ == "__main__":
-    test_generate_jsons_test7()
+    test_generate_jsons_test8()
